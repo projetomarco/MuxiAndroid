e6d0c4ddfb908deaec6894cae3275c642f6ebaad
marco <marcoacn@hotmail.com>
2017-07-14 11:56:50 -0300

    teste

---
+++ app/src/test/java/com/muxiandroid/muxiandroid/CustomizedListViewTest.java
@@ -0,0 +1,26 @@
+package com.muxiandroid.muxiandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MARCO on 14/07/2017.
+ */
+public class CustomizedListViewTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onSaveInstanceState() throws Exception {
+
+    }
+
+    @Test
+    public void getStringFromInputStream() throws Exception {
+
+    }
+
+}
